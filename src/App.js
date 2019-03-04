a3f70c6113d57b7f7f335fb7ce80b56a7c62e4f9
Maulik Shah <fraindz@gmail.com>
2019-03-04 16:36:00 +0530

    Part 2 - Add to cart, View Cart

--- src/App.js
+++ src/App.js
@@ -1,27 +1,32 @@
-import React from 'react'
-import { NavLink } from 'react-router-dom'
+import React, { Component } from 'react';
+import { NavLink, withRouter } from 'react-router-dom'
+import { connect } from 'react-redux'
+
 import Router from './Router'
 
-import './app.css'
-import './navigation.css'
+const Navigation = ({ cart }) => <nav>
+  <ul className="top-menu">
+    <li><NavLink to='/'>Home</NavLink></li>
+    <li><NavLink to='/cart'>Cart ({cart.reduce((acc, item) => {
+      return acc + item.quantity
+    }, 0)})</NavLink></li>
+    <li><NavLink to='/checkout'>Check out</NavLink></li>
+  </ul>
+</nav>
 
-const Navigation = () => (
-  <nav>
-    <ul>
-      <li><NavLink to='/'>Home</NavLink></li>
-      <li><NavLink to='/cart'>Cart</NavLink></li>
-    </ul>
-  </nav>
-)
-
-export default function App() {
-  return (
-    <div
-      className="page-container"
-    >
-      <Navigation />
-
+class App extends Component {
+  render() {
+    return <div className='page-container'>
+      <Navigation { ...this.props } />
       <Router />
     </div>
-  )
+  }
 }
+
+function mapStateToProps(state) {
+  return {
+    cart: state.cart,
+  }
+}
+
+export default withRouter(connect(mapStateToProps)(App));
--- src/Router.js
+++ src/Router.js
@@ -1,13 +1,18 @@
 import React from 'react'
 import { Switch, Route } from 'react-router-dom'
-import HomePage from './pages/home'
-import CartPage from './pages/cart'
 
-const routes = () => (
+import HomePage from './pages/homepage'
+import CartPage from './pages/cartpage'
+import CheckoutPage from './pages/checkoutpage'
+import OrdersPage from './pages/orderspage'
+
+const Router = () => (
   <Switch>
-    <Route exact path='/' component={HomePage}></Route>
-    <Route exact path='/cart' component={CartPage}></Route>
+    <Route exact path='/' component={HomePage} />
+    <Route exact path='/cart' component={CartPage} />
+    <Route exact path='/checkout' component={CheckoutPage} />
+    <Route path='/orders/:id' component={OrdersPage} />
   </Switch>
 )
 
-export default routes
+export default Router
